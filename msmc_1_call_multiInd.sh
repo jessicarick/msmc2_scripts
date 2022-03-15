e5bbcffc4287e0d15b8c812d7d65cd3372e2f367
Jessica Rick <jrick@uwyo.edu>
2022-03-15 17:44:46 +0100

    making multiind scripts

---
+++ msmc_1_call_multiInd.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# all parameters come from the msmc_param control file
+# make edits there before using this script!
+scriptdir=$(dirname "$0") # note: doesn't work with slurm wrapper!
+source ${scriptdir}/msmc_params.sh
+
+## VARIABLES:
+#IND=$1
+#BAMFILE=${BAMDIR}/aln_${IND}.sorted.bam
+BAMLIST=$1
+
+printf "\n \n \n \n"
+date
+echo "Current script: msmc_1_call.sh"
+#echo "Individual: $IND"
+echo "Bamlist: $BAMLIST"
+
+#if [ -f "${BAMFILE}.bai" ]
+#        then
+#                echo "Bamfile index already exists, moving on!"
+#        else
+#                echo "Bamfile index does not exist, creating index"
+#                samtools index $BAMFILE ${BAMFILE}.bai
+#fi
+
+
+for s in `cat SCAFFOLDS.txt`; \
+        do echo "Handling scaffold $s"; \
+#        ### Calculate mean coverage (to be used as input for bamCaller.py):
+#        MEANCOV=`samtools depth -r $s $BAMFILE | awk '{sum += $3} END {if (NR==0) print NR; else print sum / NR}' | tr ',' '.'` # calculate mean coverage
+#        echo ${IND}.${s} $MEANCOV >> ${OUTDIR}/coverage_samtoolsDepth_${IND}.txt # save mean coverage in separate file
+#        echo "Mean coverage for this individual, scaffold ${s}: $MEANCOV"
+
+#        ### Generate a single-sample VCF and a mask-file:
+#        MASK_IND=${OUTDIR}/mask/ind_mask.${IND}.${s}.${METHOD}.bed.gz # Individual mask file to be created
+        VCF=${OUTDIR}/vcf/bamlist.${s}.${METHOD}.vcf # VCF file to be created
+
+        #If genome isn't indexed, add:
+        #samtools faidx $GENOME
+        if [ "$METHOD" == "samtools" ]; then
+                echo "starting samtools alignment"
+                bcftools mpileup -Ou -r ${s} --threads 16 -f $GENOME -a INFO/AD --bam-list $BAMLIST | bcftools call -c --threads 16 -V indels | bcftools view -i 'INFO/AD>5' -Ov > ${VCF}
+
+                ## Only DP > 9:
+                #samtools mpileup -q 20 -Q 20 -C 50 -u -r $SCAFFOLD -f $GENOME $BAMFILE | bcftools call -c -V indels | bcftools view -i 'INFO/DP>9' | $MSMCTOOLS/bamCaller.py $MEANCOV $MASK_IND | gzip -c > $VCF.gz
+
+                # -q = min. mapping qual; -Q = min. base qual; -C = coefficient for downgrading mapping qual for reads w/ excessive mismatches; -u = generate uncompressed VCF/BCF; -r = only specified region; -f = fasta.
+                # bcftools: "call" will call SNPs/indels; "-V indels" skips indels; -c = consensus caller.
+        fi
+        echo "done with scaffold ${s}; moving on to next scaffold"
+done
+
+### Report:
+echo "Filtered VCF and mask created for all scaffolds for ${BAMLIST}."
+echo "Done with script."
+date
